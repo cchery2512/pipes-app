a16d6745cc625d5d21e74bf3bc316fa8268748c7
CARMELO CHERY <cchery@solucionesinformaticasmj.com>
2025-08-28 21:08:25 +0200

    PIPES 2 COMMIT

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,11 +1,85 @@
-import { Component } from '@angular/core';
+import { Component, signal } from '@angular/core';
+import { CardComponent } from "../../components/card/card.component";
+import { AsyncPipe, I18nPluralPipe, I18nSelectPipe, JsonPipe, KeyValuePipe, SlicePipe, TitleCasePipe, UpperCasePipe } from '@angular/common';
+import { interval, map, tap } from 'rxjs';
+
+const client1 = {
+  name: 'Carmelo',
+  gender: 'male',
+  age: 39,
+  address: 'Otawa, Canadá'
+};
+const client2 = {
+  name: 'Alanah',
+  gender: 'female',
+  age: 33,
+  address: 'Toronto, Canadá'
+};
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [],
+  imports: [
+    CardComponent,
+    I18nSelectPipe,
+    I18nPluralPipe,
+    SlicePipe,
+    JsonPipe,
+    UpperCasePipe,
+    KeyValuePipe,
+    TitleCasePipe,
+    AsyncPipe
+  ],
   templateUrl: './uncommon-page.component.html',
-  styles: ``
 })
 export default class UncommonPageComponent {
+  //i18n Select
+  client = signal(client1);
+
+  invitationMap = {
+    male: 'invitarlo',
+    female: 'invitarla'
+  };
+
+  changeClient(){
+    if(this.client() === client1){
+      this.client.set(client2);
+      return;
+    }
+    this.client.set(client1);
+  }
+
+  //i18n Plural
+  clients = signal(['Maria', 'Pedro', 'Fernando', 'Melissa', 'Natalia', 'Andrea', 'Juan', 'Carlos']);
+  clientsMap = signal({
+    '=0': 'no tenemos ningún cliente esperando',
+    '=1': 'tenemos un cliente esperando',
+    '=2': 'tenemos 2 clientes esperando',
+    other: 'tenemos # clientes esperando',
+  });
+  deleteClient(){
+    this.clients.update((prev) => prev.slice(1));
+  }
+
+  //KeyValue Pipe
+  profile = {
+    name: 'Aslan',
+    age: 31,
+    address: 'Sofia, Turkey'
+  };
+
+  //Async Pipe
+  promiseValue: Promise<string> = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('Tenemos data en la promesa.');
+      //console.log('Promesa finalizada');
+    }, 3500);
+  });
+
+  myObservableTimer = interval(2000).pipe(
+    map((value) => value + 1),
+    tap({
+      next: (value) => console.log('tap: ', value)
+    })
+  );
 
 }
